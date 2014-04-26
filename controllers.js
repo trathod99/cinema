69b331b64a4c7d68964e5a66fed454b858228877
Tyler <tyler.visca.barca@gmail.com>
2014-04-26 03:17:57 +0000

    fully working search over API

--- controllers.js
+++ controllers.js
@@ -2,11 +2,11 @@ angular.module('Movies.controllers', []).
 controller('movieCtrl', function($scope, $filter, tomatoAPIservice) {
         $scope.search = "";
         $scope.go = function() {
-                tomatoAPI.getMovie().success(function (data) {
-                        $scope.movie = data.movies[2].title;
-                });
                 var searchTerm = encodeURIComponent($scope.search);
                 console.log(searchTerm);
+                tomatoAPI.getMovie(searchTerm).success(function (data) {
+                        $scope.movie = data.movies[2].title;
+                });
         };
 
 });
--- services.js
+++ services.js
@@ -2,8 +2,8 @@ angular.module('Movies.services', []).
         factory('tomatoAPIservice', function ($http) {
                 tomatoAPI = []; 
                 apiKey = "skj9u6s5we4cdfcnshebqvvy"
-                tomatoAPI.getMovie  = function () {
-                        return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' + apiKey + '&q=Toy+Story&callback=JSON_CALLBACK');
+                tomatoAPI.getMovie  = function (searchTerm) {
+                        return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' + apiKey + '&q=' + searchTerm + '&callback=JSON_CALLBACK');
                 };
                 return tomatoAPI;
         });
