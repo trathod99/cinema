0de68a4bde15246d909f840553d6db751caa7e99
Tyler <tyler.visca.barca@gmail.com>
2014-04-26 02:56:18 +0000

    uri encoding works

--- controllers.js
+++ controllers.js
@@ -1,6 +1,12 @@
 angular.module('Movies.controllers', []).
-controller('movieCtrl', function($scope, tomatoAPIservice) {
-        tomatoAPIservice.getMovie().success(function (data) {
-                $scope.movie = data.movies[2].title;
-        });
+controller('movieCtrl', function($scope, $filter, tomatoAPIservice) {
+        $scope.search = "";
+        $scope.go = function() {
+                tomatoAPI.getMovie().success(function (data) {
+                        $scope.movie = data.movies[2].title;
+                });
+                var searchTerm = encodeURIComponent($scope.search);
+                console.log(searchTerm);
+        };
+
 });
