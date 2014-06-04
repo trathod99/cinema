3ff5efedf3cf45545889a3b17b48a1ea43f9ebe7
Tyler <tyler.visca.barca@gmail.com>
2014-06-04 13:06:04 -0700

    working prototype with columns

--- controllers.js
+++ controllers.js
@@ -11,18 +11,9 @@ controller('movieCtrl', function($scope, $filter, tomatoAPIservice) {
         tomatoAPI.getMovie(searchTerm).success(function (data) {
             $scope.results2 = data;
         });
-        /** if ($scope.movie === firstGreater || $scope.movie === secGreater || $scope.movie2 === firstGreater || $scope.movie2 === secGreater) {
-            $scope.movie = "";
-            $scope.movie2 = "";
-        }; **/
     };
 
-    $scope.select = function(number, results) {
-        $scope.movie = results.movies[number].ratings.audience_score;
-    };
-
-    $scope.select2 = function(number2, results2) {
-        $scope.movie2 = results2.movies[number2].ratings.audience_score;
+    $scope.compare = function() {
         if ($scope.movie > $scope.movie2) {
             $scope.firstGreater = true;
             $scope.secGreater = false;
@@ -30,10 +21,19 @@ controller('movieCtrl', function($scope, $filter, tomatoAPIservice) {
             $scope.secGreater = true;
             $scope.firstGreater = false;
         };
-    }
-
-    $scope.compare = function() {
-        
     };
+
+    $scope.select = function(number, results) {
+        $scope.movie = results.movies[number].ratings.audience_score;
+        if (typeof $scope.movie2 === 'undefined') {
+        } else {
+            $scope.compare();
+        }
+    };
+
+    $scope.select2 = function(number2, results2) {
+        $scope.movie2 = results2.movies[number2].ratings.audience_score;
+        $scope.compare();
+    }
 
 });
