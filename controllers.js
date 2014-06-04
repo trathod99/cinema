d0332e852c546889a949daf1b5f11426fa3edb38
Tyler <tyler.visca.barca@gmail.com>
2014-06-03 17:51:18 -0700

    working real-time search

--- controllers.js
+++ controllers.js
@@ -1,24 +1,26 @@
 angular.module('Movies.controllers', []).
-controller('movieCtrl', function($scope, $filter, tomatoAPIservice) {
-        $scope.search = "";
-        $scope.go = function() {
-                var searchTerm = encodeURIComponent($scope.search);
-                tomatoAPI.getMovie(searchTerm).success(function (data) {
-                        console.log(data);
-                        if (typeof $scope.movie === 'undefined') {        
-                                $scope.movie = data.movies[0].ratings.audience_score;
-                        } else {
-                                $scope.movie2 = data.movies[0].ratings.audience_score;
-                                if ($scope.movie > $scope.movie2) {
-                                        $scope.firstGreater = true;
-                                        $scope.secGreater = false;
-                                } else {
-                                        $scope.secGreater = true;
-                                        $scope.firstGreater = false;
-                                }
-                        }
-                });
-                $scope.search = "";
-        };
-
+controller('movieCtrl', function($scope, $filter, $timeout, tomatoAPIservice) {
+    $scope.search = "";
+    $scope.go = function() {
+        var searchTerm = encodeURIComponent($scope.search);
+        console.log(searchTerm);
+        tomatoAPI.getMovie(searchTerm).success(function (data) {
+            $scope.results = data;
+            /** if (typeof $scope.movie === 'undefined') {        
+                $scope.movie = data.movies[0].ratings.audience_score;
+            } else {
+                $scope.movie2 = data.movies[0].ratings.audience_score;
+                if ($scope.movie > $scope.movie2) {
+                    $scope.firstGreater = true;
+                    $scope.secGreater = false;
+                } else {
+                    $scope.secGreater = true;
+                    $scope.firstGreater = false;
+                }
+            } **/
+        });
+    };
+    $scope.select = function(number, results) {
+        $scope.movie = results.movies[number].ratings.audience_score;
+    };
 });
--- services.js
+++ services.js
@@ -1,9 +1,9 @@
 angular.module('Movies.services', []).
-        factory('tomatoAPIservice', function ($http) {
-                tomatoAPI = []; 
-                apiKey = "skj9u6s5we4cdfcnshebqvvy"
-                tomatoAPI.getMovie  = function (searchTerm) {
-                        return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' + apiKey + '&q=' + searchTerm + '&callback=JSON_CALLBACK');
-                };
-                return tomatoAPI;
-        });
+    factory('tomatoAPIservice', function ($http) {
+        tomatoAPI = []; 
+        apiKey = "skj9u6s5we4cdfcnshebqvvy"
+        tomatoAPI.getMovie  = function (searchTerm) {
+            return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' + apiKey + '&q=' + searchTerm + '&page_limit=7&page=1&callback=JSON_CALLBACK');
+        };
+        return tomatoAPI;
+    });
